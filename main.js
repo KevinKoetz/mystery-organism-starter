326ce596edaf86d563926df01774b7b3492a0548
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-05-28 11:57:47 +0200

    Move tests to tests.js

--- main.js
+++ main.js
@@ -45,16 +45,5 @@ const pAequorFactory = (ident,bases) => {
   }
 }
 
-//Test Factory
-
-const specimenOne = pAequorFactory(1, mockUpStrand());
-const specimenTwo = pAequorFactory(2, mockUpStrand());
-console.log(specimenOne);
-specimenOne.mutate();
-console.log(specimenOne);
-console.log(specimenTwo);
-specimenOne.compareDNA(specimenTwo);
-console.log(specimenOne.willLikelySurvive());
-console.log(specimenTwo.willLikelySurvive());
 
 
---
+++ tests.js
@@ -0,0 +1,13 @@
+require('./main.js');
+
+//Test Factory
+
+const specimenOne = pAequorFactory(1, mockUpStrand());
+const specimenTwo = pAequorFactory(2, mockUpStrand());
+console.log(specimenOne);
+specimenOne.mutate();
+console.log(specimenOne);
+console.log(specimenTwo);
+specimenOne.compareDNA(specimenTwo);
+console.log(specimenOne.willLikelySurvive());
+console.log(specimenTwo.willLikelySurvive());

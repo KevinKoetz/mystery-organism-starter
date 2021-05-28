8fb75f4157f1c67ef339b079e75531ee023abae5
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2021-05-28 17:25:38 +0200

    Refactor everything

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+// dnaBases global, so we can use it inside mutate()
 const dnaBases = ['A', 'T', 'C', 'G'];
+
 // Returns a random DNA base
 const returnRandBase = () => {
   return dnaBases[Math.floor(Math.random() * 4)];
@@ -13,7 +15,7 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
-const pAequorFactor = (number, array) => {
+const pAequorFactory = (number, array) => {
   return {
     specimenNum: number,
     dna: array,
@@ -22,173 +24,81 @@ const pAequorFactor = (number, array) => {
       const index = Math.floor(Math.random()*this.dna.length);
       const oldValue = this.dna[index];
       //Check which dnaBases we can use
-      /*if(oldValue === 'A') {
-        this.dna[index] = 'T';
-      } else if(oldValue === 'T'){
-        this.dna[index] = 'C';
-      } else if(oldValue === 'C'){
-        this.dna[index] = 'G';
-      }else if(oldValue === 'G'){
-        this.dna[index] = 'A';
-      */
       const possibleBases = dnaBases.filter(element => oldValue !== element);
       const newValue = possibleBases[Math.floor(Math.random() * possibleBases.length)];
       //Change the element to a random new possible element
       this.dna[index] = newValue;
     },
     compareDNA(pAequor) {
-      let numBase = 0;
+      //Number of Bases that are equal and on the same position
+      let equalBases = 0;
+      //Go through all Bases and compare with the Base on the same position in the input pAequor strand
       for (let i = 0; i < pAequor.dna.length; i++){
+        //Increment equalBases if they are the same
         if(pAequor.dna[i] === this.dna[i]) {
-          numBase += 1;
+          equalBases++;
         }
       }
-      let percentage = Math.floor(numBase/pAequor.dna.length*100);
+      let percentage = equalBases/pAequor.dna.length*100;
       console.log(`Specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${percentage}% DNA in common`);
+      //Return percentage to be used when finding the pAequor closest related to another
       return percentage;
     },
     willLikelySurvive(){
-      /*
-      const percentage = this.dna.reduce((numBase, ele) => {
-        numBase = numBase * this.dna.length / 100;
-        if(ele === 'C' || ele === 'G') numBase++;
-        return numBase/this.dna.length*100;
-      }, 0)
-      return (percentage >= 60)
-      */
-      
-      let numBase = 0;
-      for (let i = 0; i < this.dna.length; i++){
-        if(this.dna[i] === 'C') {
-          numBase += 1;
-        }
-        if(this.dna[i] === 'G') {
-          numBase += 1;
-        }
-      }
-      return (numBase/this.dna.length*100 >= 60)
-     
+      //Reduce the DNA Strand to a single percentage Value. 
+      const percentage = this.dna.reduce((percentage, base) => {
+        //Convert the percentage Value to the number of bases that are C or G
+        goodBases = percentage * this.dna.length / 100;
+        //If base is C or G, add a good Base
+        if(base === 'C' || base === 'G') goodBases++;
+        //Calculate the percentage of bases that are C or G
+        return goodBases/this.dna.length*100;
+      },0)
+      return (percentage >= 60)    
     },
     complementStrand(){
-      return this.dna.map(el => 
-      el === 'A' ? 'T' : 
-      el === 'T' ? 'A' : 
-      el === 'C' ? 'G' : 
-      el === 'G' ? 'C' : undefined)
-
-      /*
-      const complementStrand = [];
-      let strand1 = this.dna;
-      for (let i = 0; i < strand1.length; i++) {
-        switch(strand1[i]){
-          case 'A':
-            complementStrand.push('T')
-            break;
-          case 'T':
-            complementStrand.push('A')
-            break;
-          case 'C':
-            complementStrand.push('G')
-            break;
-          case 'G':
-            complementStrand.push('C')
-            break;
-        }
-        
-        if(strand1[i] === 'A') {
-          complementStrand.push('T')
-        } else if (strand1[i] === 'T') {
-          complementStrand.push('A')
-        } else if (strand1[i] === 'G') {
-          complementStrand.push('C')
-        } else if (strand1[i] === 'C') {
-          complementStrand.push('G')
-        }
-        
-      }
-      return complementStrand;*/
-      
+      return this.dna.map(base =>
+      //Switch A to T
+      base === 'A' ? 'T' : 
+      //Switch T to A
+      base === 'T' ? 'A' : 
+      //Switch C to G
+      base === 'C' ? 'G' : 
+      //Switch G to C, or return undefined if the Base is "something" different
+      base === 'G' ? 'C' : undefined)     
     }
   }
 }
 
-let mutantArray = [];
-while (mutantArray.length < 30) {
-  let mutant = pAequorFactor(mutantArray.length, mockUpStrand());
-  if(mutant.willLikelySurvive()) mutantArray.push(mutant);
+//Create 30 pAequors with random Strands
+let pAequors = [];
+while (pAequors.length < 30) {
+  let pAequor = pAequorFactory(pAequors.length, mockUpStrand());
+  if(pAequor.willLikelySurvive()) pAequors.push(pAequor);
 }
 
+//Returns the most related Pair of pAequors out of a sample
+const getMostRelated = (pAequors) => {
+  //The Most Related Pair of pAequors are saved in this object.
+  const mostRelated = {
+    firstPAequor: pAequors[0],
+    secondPAequor: pAequors[1],
+    percentage: pAequors[0].compareDNA(pAequors[1])
+  }
 
-const mostRelated = (array) => {
-  let comparisons = []; 
-  for(let i = 0; i < array.length; i++){
-    for(let j = 0; j < array.length; j++)
-      if(i !== j){
-        comparisons.push({
-          first: array[i],
-          second: array[j],
-          percentage: (array[i].compareDNA(array[j]))
-        }
-        )
+  //Go through all pAequors and compare them with each other
+  for(let i = 0; i < pAequors.length; i++){
+    //We only need to compare against each pAequor with a higher index
+    for(let j = i + 1; j < pAequors.length; j++)
+      if(pAequors[i].compareDNA(pAequors[j]) > mostRelated.percentage){
+        mostRelated.firstPAequor = pAequors[i];
+        mostRelated.secondPAequor = pAequors[j];
+        mostRelated.percentage = pAequors[i].compareDNA(pAequors[j]);
       }
   }
-  let sortedComparisons = comparisons.sort(
-    (first, second) => {
-      if (second.percentage < first.percentage) {
-        return -1;
-      }
-      if (second.percentage > first.percentage) {
-        return 1;
-      }
-      // a must be equal to b
-      return 0;
-    }
-  )
-  //console.log(sortedComparisons);
-  return sortedComparisons[0]; 
+  console.log(`Specimen #${mostRelated.firstPAequor.specimenNum} and specimen #${mostRelated.secondPAequor.specimenNum} are most related and have ${mostRelated.percentage}% DNA in common`)
+  return mostRelated; 
 }
 
 
-//Tests
-
-const pAequor1 = pAequorFactor(1, mockUpStrand());
-const pAequor2 = pAequorFactor(2, mockUpStrand());
-const pAequor3 = pAequorFactor(3, mockUpStrand());
-const pAequor4 = pAequorFactor(4, mockUpStrand());
-
-pAequor1.dna = ['A', 'A', 'A', 'A']; 
-pAequor2.dna = ['G', 'G', 'G', 'G']; 
-pAequor3.dna = ['G', 'G', 'G', 'G']; 
-pAequor4.dna = ['G', 'G', 'G', 'G']; 
-
-const testSample = [pAequor1, pAequor2, pAequor3, pAequor4];
-console.log(mostRelated(testSample));
-
-/*
-const pAequor1 = pAequorFactor(1, mockUpStrand());
-const pAequor2 = pAequorFactor(2, mockUpStrand());
-
-
-pAequor1.dna = ['A', 'G', 'C', 'T']; 
-pAequor2.dna = ['A', 'G', 'C', 'T']; 
-pAequor1.compareDNA(pAequor2); //Should print 100% common DNA
-pAequor2.dna = ['A', 'G', 'A', 'A']; 
-pAequor1.compareDNA(pAequor2); //Should print 50% common DNA
-
-pAequor1.dna = ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A']; 
-console.log(pAequor1.willLikelySurvive());
-pAequor1.dna = ['C', 'C', 'C', 'C', 'C', 'C', 'A', 'A', 'A', 'A']; 
-console.log(pAequor1.willLikelySurvive());
-pAequor1.dna = ['G', 'G', 'G', 'G', 'G', 'G', 'A', 'A', 'A', 'A']; 
-console.log(pAequor1.willLikelySurvive());
-*/
-/*
-console.log(mutantArray.length);
-for(const mutant of mutantArray){
-  console.log(mutant.willLikelySurvive());
-}*/
-/*
-const pAequor1 = pAequorFactor(1, mockUpStrand());
-pAequor1.dna = ['A', 'G', 'C', 'T']; 
-console.log(pAequor1.complementStrand()); //T C G A
-*/
+console.log(getMostRelated(pAequors));
